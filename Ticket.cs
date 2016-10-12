c1056b24004d314f046a8a3b19f611eee7d90263
Kyllian Franken <kyllian__1993@hotmail.com>
2016-10-13 00:29:09 +0200

    new

---
+++ Ticket.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    public abstract class Ticket
+    {
+        string Name;
+        string departure;
+        string destination;
+        bool firstClass;
+        int discount;
+
+        public abstract int getPrice();
+        public abstract void makeTicket(string from, string to, bool first, int discountPercent);
+    }
+
+    public class NormalTicket : Ticket
+    {
+        string Name;
+        string departure;
+        string destination;
+        bool firstClass;
+        int discount;
+
+        public override int getPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void makeTicket(string from, string to, bool first, int discountPercent)
+        {
+            Name = from + " to " + to;
+            departure = from;
+            destination = to;
+            firstClass = first;
+            discount = discountPercent;
+        }
+    }
+
+}
---
+++ TicketOrder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3
+{
+    class TicketOrder
+    {
+        List<Ticket> allTickets;
+        int totalPrice;
+
+        public void addNormalTicket(string from, string to, bool firstClass, int discountPercent)
+        {
+            NormalTicket newTicket = new NormalTicket();
+            newTicket.makeTicket(from, to, firstClass, discountPercent);
+            allTickets.Add(newTicket);
+        }
+
+        public float getTotal()
+        {
+            totalPrice = 0;
+            foreach(Ticket a in allTickets)
+            {
+                totalPrice += a.getPrice();
+            }
+            return totalPrice;
+        }
+
+        public void makePayment()
+        {
+            switch (info.Payment)
+            {
+                case UIPayment.CreditCard:
+                    CreditCard c = new CreditCard();
+                    c.Connect();
+                    int ccid = c.BeginTransaction(getTotal());
+                    c.EndTransaction(ccid);
+                    break;
+                case UIPayment.DebitCard:
+                    DebitCard d = new DebitCard();
+                    d.Connect();
+                    int dcid = d.BeginTransaction(getTotal());
+                    d.EndTransaction(dcid);
+                    break;
+                case UIPayment.Cash:
+                    IKEAMyntAtare2000 coin = new IKEAMyntAtare2000();
+                    coin.starta();
+                    coin.betala((int)Math.Round(getTotal() * 100));
+                    coin.stoppa();
+                    break;
+            }
+        }
+    }
+    }
+}
